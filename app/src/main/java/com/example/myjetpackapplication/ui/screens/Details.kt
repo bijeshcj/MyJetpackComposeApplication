56b52b2c84e1607e5c76f52aff75d1ef3710f4d2
bijaychanadikaljayaprakash <bijesh.jayaprakash@circles.asia>
2023-04-09 12:31:26 +0530

    cheezy code : mutableState of Int

--- app/src/main/java/com/example/myjetpackapplication/ui/screens/Details.kt
+++ app/src/main/java/com/example/myjetpackapplication/ui/screens/Details.kt
@@ -1,37 +1,76 @@
 package com.example.myjetpackapplication.ui.screens
 
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.Text
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
 
 @Composable
-fun DetailsScreen(navController: NavController){
-     Box(modifier = Modifier.fillMaxSize(),
-     contentAlignment = Alignment.Center){
-         Text(text = "Details",
-             modifier = Modifier.clickable {
-                 navController.popBackStack()
-             },
-         color = Color.Green,
-         fontSize = 20.sp,
-         fontWeight = FontWeight.Bold
-         )
-     }
+fun DetailsScreen(navController: NavController) {
+    var count = remember { mutableStateOf(0) }
+    Box(
+        modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center,
+    ) {
+        Column(
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally,
+        ) {
+            Text(
+                text = "Sent the number of notification ${count.value}",
+                color = Color.Black,
+            )
+
+            Button(onClick = { count.value++ }) {
+                Text(text = "Send Notification")
+            }
+
+            MessageCard(count.value)
+
+            Text(
+                text = "Details",
+                modifier = Modifier.clickable {
+                    navController.popBackStack()
+                },
+                color = Color.Green,
+                fontSize = 20.sp,
+                fontWeight = FontWeight.Bold,
+            )
+        }
+    }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun MessageCard(count: Int) {
+    Card(
+        shape = RoundedCornerShape(8.dp),
+        elevation = CardDefaults.cardElevation(defaultElevation = 4.dp),
+
+    ) {
+        Row(modifier = Modifier.padding(8.dp)) {
+            Icon(imageVector = Icons.Default.Favorite, contentDescription = "Favorite")
+            Text(text = "Messages sent so far - $count")
+        }
+    }
+}
 
 @Preview(showBackground = true)
 @Composable
-fun DetailsScreenPreview(){
+fun DetailsScreenPreview() {
     DetailsScreen(rememberNavController())
 }
